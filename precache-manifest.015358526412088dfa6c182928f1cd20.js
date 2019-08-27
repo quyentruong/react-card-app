c4d9265e1140521ab7dce3297ea3cd70aabeb929
Quyen Truong <Quyen.Truong@CodeSmartInc.com>
2019-08-27 11:05:46 -0700

    Updates

---
+++ precache-manifest.015358526412088dfa6c182928f1cd20.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb8bd7b1f2b9922bf4378dc02ea78b50",
+    "url": "/react-card-app/index.html"
+  },
+  {
+    "revision": "358fef4d17d374d39ecc",
+    "url": "/react-card-app/static/css/main.93db5191.chunk.css"
+  },
+  {
+    "revision": "95165c312218e3991ceb",
+    "url": "/react-card-app/static/js/2.b0a62e96.chunk.js"
+  },
+  {
+    "revision": "358fef4d17d374d39ecc",
+    "url": "/react-card-app/static/js/main.79c83502.chunk.js"
+  },
+  {
+    "revision": "e520f7ef1b8d3a95bc9e",
+    "url": "/react-card-app/static/js/runtime~main.a7bf26a8.js"
+  }
+]);
